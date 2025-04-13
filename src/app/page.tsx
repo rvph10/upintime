64b77509fac3bf20db4002fce4f8bd1af2852406
rvph10 <rgenu10@gmail.com>
2025-04-14 01:58:24 +0200

    feature: added faveico

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,7 @@
 export default function Home() {
   return (
     <main>
-       <div className="w-full h-full rounded-lg overflow-hidden">
-      </div>
+      <div className="w-full h-full rounded-lg overflow-hidden"></div>
     </main>
   );
 }
--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,24 +1,48 @@
-import React from 'react'
-import Image from 'next/image'
-import Link from 'next/link'
-
+import React from "react";
+import Image from "next/image";
+import Link from "next/link";
 
 function Header() {
   return (
-    <div className='flex justify-between items-center text-foreground'>
+    <div className="flex justify-between items-center text-foreground">
       {/* Logo */}
-      <Link href="/" >
-        <Image src="/upintown.svg" alt="Up In Town Logo" width={48} height={48}/>
+      <Link href="/">
+        <Image
+          src="/upintown.svg"
+          alt="Up In Town Logo"
+          width={48}
+          height={48}
+        />
       </Link>
       {/* Navbar */}
-      <div className='flex items-center gap-12 font-medium'>
-        <Link href="/" className='hover:text-foreground/40 transition-all duration-300'>Home</Link>
-        <Link href="/work" className='hover:text-foreground/40 transition-all duration-300'>Work</Link>
-        <Link href="/about" className='hover:text-foreground/40 transition-all duration-300'>About</Link>
-        <Link href="/contact" className='hover:text-foreground/40 transition-all duration-300'>Contact</Link>
+      <div className="flex items-center gap-12 font-medium">
+        <Link
+          href="/"
+          className="hover:text-foreground/40 transition-all duration-300"
+        >
+          Home
+        </Link>
+        <Link
+          href="/work"
+          className="hover:text-foreground/40 transition-all duration-300"
+        >
+          Work
+        </Link>
+        <Link
+          href="/about"
+          className="hover:text-foreground/40 transition-all duration-300"
+        >
+          About
+        </Link>
+        <Link
+          href="/contact"
+          className="hover:text-foreground/40 transition-all duration-300"
+        >
+          Contact
+        </Link>
       </div>
     </div>
-  )
+  );
 }
 
-export default Header
+export default Header;
