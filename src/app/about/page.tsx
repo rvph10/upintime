28de8445996213fdbae6808b23171f66bf5c0ae1
rvph10 <rgenu10@gmail.com>
2025-04-24 15:15:00 +0200

    feat: added sample page

---
+++ src/app/about/page.tsx
@@ -0,0 +1,10 @@
+"use client";
+export default function AboutPage() {
+  return (
+    <div className="w-full min-h-screen flex items-center justify-center">
+      <div className="w-full text-center font-bold text-4xl md:text-6xl lg:text-9xl text-foreground flex flex-col items-center justify-center px-4">
+        <h1 className="transform translate-y-8 pb-6">About</h1>
+      </div>
+    </div>
+  );
+}
---
+++ src/app/contact/page.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Contact = () => {
+  return (
+    <div className="w-full min-h-screen flex items-center justify-center">
+      <div className="w-full text-center font-bold text-4xl md:text-6xl lg:text-9xl text-foreground flex flex-col items-center justify-center px-4">
+        <h1 className="transform translate-y-8 pb-6">Contact</h1>
+      </div>
+    </div>
+  );
+};
+
+export default Contact;
